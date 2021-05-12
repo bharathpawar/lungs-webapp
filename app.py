06d0b052e2673f328021aeb23ac43227f2c09520
ELL KAY <lakshay912.lk@gmail.com>
2021-05-12 14:58:30 +0530

    maybe last

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from gevent.pywsgi import WSGIServer
 app = Flask(__name__)
 
 # Model saved with Keras model.save()
-MODEL_PATH = 'models/model_resnet.h5'
+MODEL_PATH = 'models/xray_model.h5'
 print(" MODEL_PATH :",MODEL_PATH)
 #Load your trained model
 model = load_model(MODEL_PATH)
